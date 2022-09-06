c380833af183385d7314e28b81ab5a4821539487
mawwk <>
2022-09-06 00:00:18 -0500

    Rename themes to "The Construct", "Invincible 6." Darken unselected lines. Code cleanup

--- PowerPC Assembly Functions/Code Menu.cpp
+++ PowerPC Assembly Functions/Code Menu.cpp
@@ -334,7 +334,7 @@ void CodeMenu()
 
 	MainLines.push_back(new Comment(""));
 	MainLines.push_back(new Selection("Stagelist", { "Default", "Singles (WI)", "Doubles (WI)",  "Singles (PMBR)", "Doubles (PMBR)", "Singles (Theatre)", "Doubles (Theatre)" }, 0, STAGELIST_INDEX));
-	MainLines.push_back(new Selection("Theme", { "WI", "Construct", "Invincible" }, 0, THEME_INDEX));
+	MainLines.push_back(new Selection("Theme", { "WI", "The Construct", "Invincible 6" }, 0, THEME_INDEX));
 	MainLines.push_back(&SpecialSettingsPage.CalledFromLine);
 	MainLines.push_back(&PlayerCodes.CalledFromLine);
 	
--- PowerPC Assembly Functions/MainCode.cpp
+++ PowerPC Assembly Functions/MainCode.cpp
@@ -25,81 +25,10 @@ int main()
 	string OutputTextPath = "C:\\Users\\m\\Desktop\\Wisco PM\\wisco build P+\\asm.txt";
 	
 	CodeStart(OutputTextPath);
-	//place all ASM code here
-
-	//ReplayFix();
-
-	//NameIsFound();
-
-	//MenuControlCodes();
-
-	//StopStartAltFunctions();
-
-	//StopPokemonTrainerSwitch();
-
-	//StopDPadOnSSS();
-
-	//ConvertButtons();
-
-	//ItemSpawnControl();
-
-	//ClearASLData();
-
-	//SetTeamAttackTraining();
-
-	//LXPGreenOverlayFix();
 
 	CodeMenu(); tagBasedCostumes();
 
-	//musicPercentCode();
-
-	//DoubleFighterTest();
-
-	//UCF();
-
-	//CStickSlowFix();
-
-	//FixStickyRAlts();
-
-	//SelectLastCharacter();
-
-	//FixTr4shTeamToggle();
-
-	//cstickTiltTest();
-
-	//FPSDisplay();
-
-	//CStickTiltFix();
-
-	//DBZModeTest();
-
-	//slipperyTechs();
-
-	//lightShield();
-
-	//IkeClimbers();
-
-	//fixStanimaTextBug();
-
-	//AIDisplay();
-
-	//loadCppCodes(); writeInjectionsRepeat();
-
 	CodeEnd();
 
-
-
-	//string InputGCTPath = "C:\\Users\\john\\Desktop\\ASM Testing\\Good GCT\\RSBE01.gct";
-	//string InputGCTPath = "C:\\Users\\john\\Downloads\\Legacy TE 1.0\\Legacy TE 1.0\\LegacyTE\\RSBE01.gct";
-	//string InputGCTPath = "C:\\Users\\john\\Downloads\\Legacy TE 1.11 Wii\\Legacy TE 1.11 Wii\\LegacyTE\\RSBE01.gct";
-	//string InputGCTPath = "C:\\Users\\john\\Downloads\\Legacy TE\\LegacyTE\\RSBE01.gct"s;
-#if BUILD_TYPE == PROJECT_PLUS
-	string InputGCTPath = R"(C:\Users\johno\Downloads\tournament-addition-master\Project+\RSBE01.GCT)";
-#elif BUILD_TYPE == NORMAL
-	string InputGCTPath = "C:\\Users\\john\\Downloads\\legacyte-master-Smash Bros Legacy TE-Legacy TE\\Smash Bros Legacy TE\\Legacy TE\\LegacyTE\\RSBE01.gct";
-#endif
-	//string InputGCTPath = "C:\\Users\\john\\Downloads\\LXPL 2.1 v1 [Wii]\\LegacyLT\\RSBE01.gct";
-	string OutputGCTPath = R"(C:\Users\johno\Documents\Modding\Dolphin\SD_Raws\RSBE01.gct)";
-	//MakeGCT(OutputTextPath, InputGCTPath, OutputGCTPath); //remove if you don't want to append code
 	return 0;
 }
