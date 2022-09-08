6494645c4f888bc0d1a1ea088e4c3569abd40832
mawwk <>
2022-09-08 00:07:01 -0500

    Version number bump to 2.31

--- PowerPC Assembly Functions/Code Menu.cpp
+++ PowerPC Assembly Functions/Code Menu.cpp
@@ -308,9 +308,9 @@ void CodeMenu()
 	//main page
 	vector<Line*> MainLines;
 #if DOLPHIN_BUILD
-	MainLines.push_back(new Comment("WI Netplay Code Menu (P+ 2.3)", &MENU_TITLE_CHECK_LOCATION));
+	MainLines.push_back(new Comment("WI Netplay Code Menu (P+ 2.31)", &MENU_TITLE_CHECK_LOCATION));
 #else
-	MainLines.push_back(new Comment("WI Code Menu (P+ 2.3)", &MENU_TITLE_CHECK_LOCATION));
+	MainLines.push_back(new Comment("WI Code Menu (P+ 2.31)", &MENU_TITLE_CHECK_LOCATION));
 #endif
 	//MainLines.push_back(new Comment("X = Reset Line | Y = Reset Page"));
 	//MainLines.push_back(new Comment("Hold Z = Scroll Faster"));
