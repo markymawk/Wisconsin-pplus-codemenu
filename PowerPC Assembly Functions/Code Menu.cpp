de0d8f88afa4856187e5ba4493166aacf24e2472
mawwwk <>
2024-05-20 19:42:45 -0500

    Update header text

--- PowerPC Assembly Functions/Code Menu.cpp
+++ PowerPC Assembly Functions/Code Menu.cpp
@@ -938,7 +938,7 @@ void CodeMenu()
 
 	//main page
 	vector<Line*> MainLines;
-	MainLines.push_back(new Comment("Inv8 Code Menu v1.6b (P+ 2.5.2)", &MENU_TITLE_CHECK_LOCATION));
+	MainLines.push_back(new Comment("Inv8 Code Menu (P+ 2.5.2)", &MENU_TITLE_CHECK_LOCATION));
 	MainLines.push_back(new Comment("start.gg/invincible | @WiscoPM", &MENU_TITLE_CHECK_LOCATION));
 
 	MainLines.push_back(new Comment(""));
